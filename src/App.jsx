175989fff159294a787c359cda23093c46e33d79
Arghya <arghyaghosho48@gmail.com>
2024-03-21 23:45:42 +0530

    created routes and organise the file

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Dashboard } from './routes/Dashboard'
+
+
+function App() {
+  
+
+  return (
+  <>
+  <BrowserRouter>
+  <Routes>
+  {/* <Route path="/signup" element={<SignUp></SignUp>}></Route>
+  <Route path="/signin" element={<SignIn></SignIn>}></Route> */}
+  <Route path='/maildashboard' element={<Dashboard></Dashboard>}></Route>
+  {/* <Route path='/sendmail' element={<SendMail></SendMail>}></Route> */}
+
+  </Routes>
+  
+  </BrowserRouter>
+  
+  
+  </>
+   
+  )
+}
+
+export default App
---
+++ src/routes/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from '../component/Header'
+
+export const Dashboard = () => {
+  return (<><div>
+    <Header/>
+</div></>
+    
+  )
+}
